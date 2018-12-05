fee69b19c97c3159c9929478893fa16b006323bf
Maciej <maciej.dra@gmail.com>
2018-12-05 09:05:02 +0100

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 public class Main {
 
+    static class Value{
+
+        int x;
+
+    }
+
     public void changeValue(Value val ){
+
         val.x = 10;
+
     }
 
     public static void main(String[] args) {
+
         Main t = new Main();
         Value wartosc = new Value();
         wartosc.x = 5;
         t.changeValue(wartosc);
         System.out.println(wartosc.x);
-    }
 
-    static class Value{
-        int x;
     }
 }
